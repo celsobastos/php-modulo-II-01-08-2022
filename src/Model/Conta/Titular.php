393265cea652a3496410fa8c6567c8c98f46656d
Celso Bastos <celsorico@gmail.com>
2022-08-08 19:04:58 -0300

    POO

--- src/Model/Conta/Titular.php
+++ src/Model/Conta/Titular.php
@@ -12,7 +12,7 @@ class Titular extends Pessoa implements AutenticarInterface{
         $login_banco = 'sergio@bancoimpacta.com';
         $senha_banco = '1234';
         if($login_banco === $login && $senha_banco === $senha) {
-            echo "$this->getNome(), Autenticado com sucesso";
+            return "$this->getNome(), Autenticado com sucesso";
         }
 
         throw new \Exception("Ops! $this->getNome(), Autenticação Falhou");
